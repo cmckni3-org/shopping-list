66eb7d158d7a2f7ffd35bbf85b4d355b88733dd1
Chris McKnight <cmckni3@gmail.com>
2015-04-20 21:50:40 -0500

    * Removes serve-favicon * Adds mocha

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express      = require('express');
 var path         = require('path');
-var favicon      = require('serve-favicon');
 var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
@@ -27,11 +26,11 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('hbs', expressHbs({extname: 'hbs', defaultLayout: 'main.hbs'}));
+// Using work around compilerOptions: undefined for now.
+// This was changed in handlebars version 3.0.2 with ES 2015 support
+app.engine('hbs', expressHbs({extname: 'hbs', defaultLayout: 'main.hbs', compilerOptions: undefined}));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
---
+++ test/support/helpers.js
@@ -0,0 +1,6 @@
+var chai = require('chai'),
+    chai_http = require('chai-http');
+
+var should = chai.should();
+chai.use(chai_http);
+
